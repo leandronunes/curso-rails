32ab0fa4c2662c043a8bed227df45e68a2e1ac3d
Leandro Nunes dos Santos <leandronunes@gmail.com>
2012-10-17 02:16:03 -0300

    adding validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+  before_filter :authenticate, :except => [:index, :show]
+
   # GET /posts
   # GET /posts.xml
   def index
@@ -81,6 +84,14 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to(posts_url) }
       format.xml  { head :ok }
+    end
+  end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |name, pass|
+      name == 'admin' && pass == 'admin'
     end
   end
 
