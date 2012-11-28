b72628356bdf84e9df7470775725451c581b21a8
Leandro Nunes dos Santos <leandro.santos@serpro.gov.br>
2012-11-28 12:52:13 -0300

    adding ajax support for add and remove comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,19 +3,36 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(params[:comment])
-    @comment.save
-    redirect_to @post
+    respond_to do |format|
+      if @comment.save
+        flash[:notice] = 'Comment was successfully created.'
+        format.js  {
+          render :update do |page|
+            page.replace_html 'comments', :partial => @post.comments
+            page.visual_effect :highlight, 'comments'
+          end
+        }
+
+      else
+        format.js  {
+          render :update do |page|
+            page.replace_html 'comments', 'Something wrong happened o comment creation'
+            page.visual_effect :highlight, 'comments'
+          end
+        }
+      end
+    end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.xml
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-
     respond_to do |format|
-      format.html { redirect_to(comments_url) }
-      format.xml  { head :ok }
+      format.js  {
+        render :update do |page|
+          page.remove 'comment_' + params[:id]
+        end
+      }
     end
   end
 end
